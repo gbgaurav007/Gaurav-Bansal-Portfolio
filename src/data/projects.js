704921215b7c06a66eccf76cc91bbb2adff4a88c
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2025-06-05 02:59:25 +0530

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+const projects = [
+    {
+      title: "EcoRide",
+      description: "A carpooling platform to search and book shared rides",
+      imageUrl: "assets/p1.jpg",
+      githubLink: "https://github.com/gbgaurav007/EcoRide",
+      liveLink: "https://eco-ride.vercel.app"
+    },
+    {
+      title: "EventCraft",
+      description: "A Role-Based Acces Control Event Management System",
+      imageUrl: "assets/p2.jpeg",
+      githubLink: "https://github.com/gbgaurav007/EventCraft",
+      liveLink: "https://event-craft.vercel.app"
+    },
+    {
+      title: "NeoVision",
+      description: "A user-centric VR Headset website with a comprehensive product catalog",
+      imageUrl: "assets/p3.webp",
+      githubLink: "https://github.com/gbgaurav007/NeoVision",
+      liveLink: "https://neo-vision-zeta.vercel.app/mainpage.html"
+    }
+  ];
+
+  export default projects;
