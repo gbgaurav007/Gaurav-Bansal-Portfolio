c566755791dd4994fd7189123e59ca43784188c4
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2025-06-05 23:59:15 +0530

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -8,7 +8,7 @@ const experiences = [
     description:
       'Gained comprehensive hands on experience in iOS app development. Developed a fully functional SwiftUI based Library Management app, with RESTful API integration enabling seamless data synchronisation between the app and the backend. Collaborated with cross-functional teams to ensure UI/UX alignment and smooth database integration. Adopted Agile methodologies for iterative development while maintaining code quality and scalable architecture.',
     tech: ['Swift', 'REST APIs', 'JavaScript', 'Database'],
-    logo: 'assets/infosys.jpeg',
+    logo: 'assets/infosys.jpg',
   },
 ];
 
